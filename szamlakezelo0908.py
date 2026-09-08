92581e6f095eb37a95752b138e2ca37f3ac7c2eb
csdavsuli <22b-csik.david@asbothkeszthely.hu>
2026-09-08 09:58:21 +0200

    0908

---
+++ szamlakezelo0908.py
@@ -0,0 +1,26 @@
+#SZÁMLAKEZELÉS
+
+#GLOBAL VALTOZOK
+egyenleg=0
+pin=1234
+hasznalatidij=1000
+adatfajl="szamla.txt"
+jogosult=False
+
+
+#MŰKÖDÉS
+hibasbelepes=3
+
+pk=int(input("Kérem adja meg a PIN kódot: "))
+if pk==pin:
+        jogosult=True
+        print("Jogosultság ellenőrzése sikeres.")
+
+while(jogosult==False and hibasbelepes>0):
+    pk=int(input("Kérem adja meg a PIN kódot: "))
+    print("Jogosultság ellenőrzése sikertelen.")
+
+    hibasbelepes-=1
+    if pk==pin:
+        jogosult=True
+        print("Jogosultság ellenőrzése sikeres.")
